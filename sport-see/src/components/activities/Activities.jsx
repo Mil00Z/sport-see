bffd5754e7330dae43ecdbcf7c8e73d42c2455be
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-20 18:22:49 +0100

    Add Activities assets looper

--- sport-see/src/components/activities/Activities.jsx
+++ sport-see/src/components/activities/Activities.jsx
@@ -1,20 +1,38 @@
+import yoga from '@assets/yoga.svg'
+import swim from '@assets/swim.svg'
+import bike from '@assets/bike.svg'
+import dumbell from '@assets/dumbell.svg'
+
 import '@styles/layout/activities.scss'
 
 const Activities = () => {
 
+  const icons = [yoga, swim, bike, dumbell ]
 
-
+  let dateTime = new Date();
+  let months =  dateTime.toLocaleString('default', { month: 'long' });
+  let years = dateTime.getFullYear();
 
   return (
     <aside className='activities'>
       <nav className='second-nav'>
         <ul className="activities-list">
-          <li className="truc 1"><a className="item" href="/"></a></li>
-          <li className="truc 2"><a className="item" href="/"></a></li>
-          <li className="truc 3"><a className="item" href="/"></a></li>
-          <li className="truc 4"><a className="item" href="/"></a></li>
+
+          {icons.map((icon, index) => {
+              return ( 
+              <li key={`icon-${index}`}>
+                <a className="item-icon" href="/" aria-label="Lien vers l'activité">
+                  <img className="" src={icon} alt={`Icone de l'activité ${index}`} aria-label="" />
+                  </a>
+              </li>
+              )
+
+          })
+        }
+
         </ul>
       </nav>
+      <p className="copyright">Copyright, <a href="https://github.com/Mil00Z/sport-see" target='_blank'data-realase={months}>Mil00Z</a> {years}</p>
     </aside>
   )
 
