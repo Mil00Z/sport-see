2167b7bbae9ff15709dc9658c9904c5b151fe5ca
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-20 18:23:00 +0100

    add main logo

--- sport-see/src/components/navigation/Navigation.jsx
+++ sport-see/src/components/navigation/Navigation.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import logo from '@assets/logo.svg'
 
 import '@styles/layout/navigation.scss'
 
@@ -7,6 +8,7 @@ const Navigation = () => {
 
   return ( 
     <nav className='main-nav'>
+      <img className={`logo-${document.title}`} src={logo} alt="logo de Sport See" />
       <ul>
         <li><Link to="/" className="link">Accueil</Link></li>
         <li><Link to="/profil" className="link">Profil</Link></li>
