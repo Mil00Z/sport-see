62350528916ab77b3b00bdc78a74e29b7014dfd0
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-20 13:10:47 +0100

    Starter Flow React with Root

--- sport-see/src/main.jsx
+++ sport-see/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Root from '@components/Root'
+import Home from '@components/home/Home'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
