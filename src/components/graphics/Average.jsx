416eb80889e288797af6ef3379fa43be0e62af8d
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-26 17:56:16 +0100

    Update Average component with gestion of not fetching data + use state and effect

--- src/components/graphics/Average.jsx
+++ src/components/graphics/Average.jsx
@@ -1,38 +1,94 @@
+import {useState,useEffect} from 'react';
 import mockAverage from '@datas/mock/mockUser12Average.json'
 
+import Error from '@components/error/Error';
+
+import { AreaChart, Area, XAxis,Tooltip, ResponsiveContainer,Label } from 'recharts'
 
 import '@styles/layout/graphics.scss'
 
 const Average = () =>{
 
-  let averageDatas = mockAverage[0];
+  let dataAverage = mockAverage[0];
 
-  // let averageDatas = mockAverage.data
+  const [dataLife,setDataLife] = useState(false);
 
-  console.log(averageDatas);
+  const [dataSets,setDataSets] = useState([]);
+
+
+
+  useEffect(() => { 
+
+    if (dataAverage) {
+
+      setDataLife(dataLife=>!dataLife);
+
+      setDataSets(dataSets => {
+
+        return dataAverage.sessions.map((item) => {
+  
+          return {
+            "name": item.day,
+            "value": item.sessionLength,
+          }
+    
+      })
+
+    });
+  
+    
+    } else {
+  
+      console.error('No data available to display');
+  
+    }
+
+  },[dataAverage]);
+
+
+
+ const CustomTooltip = ({ active, payload}) => {
+
+    if (active && payload && payload.length) {
+
+      // console.log(payload)
+
+      return (
+          <div className="custom-tooltip">
+              <p className="label">{`${payload[0].value} min`}</p>
+          </div>
+      )
+  } 
+}
 
 
   return ( 
   
-    <div className="block average" data-user={averageDatas.userId}>
+    <div className="block average" data-user={dataAverage?.userId}>
 
-      {averageDatas.sessions.map((item, index) => {
+      {dataAverage ? (
 
-      return(
-            <div key={`date-${index}`} className="dates">
-              <span>Day : {item.day}</span>
-              <span>Durée : {item.sessionLength}</span>
-            </div>
+        <ResponsiveContainer width="100%" height="100%">
 
-          )
+          <AreaChart data={dataSets} margin={{ top: 0, right: 20, bottom: 0, left: 20 }}>
 
-
-        })} 
-
-
-
+          <Label value="Pages of my website" offset={0} position="insideBottom" />
           
+            <XAxis dataKey="name" stroke="var(--white-color)" tickLine={false} axisLine={false} tickFormatter={(tick) => {
+               const days = ["L", "M", "M", "J", "V", "S", "D"];
+               return days[tick - 1];
+               }}/>
+            <Tooltip content={<CustomTooltip />}/>  
 
+            <Area type="monotone" dataKey="value" stroke="var(--white-color)" fillOpacity={1} fill="var(--primary-color)" />
+
+          </AreaChart>
+        </ResponsiveContainer>
+
+        ) : (<Error dataLife={dataLife} />)
+
+
+      }
 
     </div>
   
