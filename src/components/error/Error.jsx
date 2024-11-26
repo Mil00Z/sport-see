a285bbeabff3f39aa7e7b18f3c72fb727dbb25f0
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-26 17:55:03 +0100

    Create Error Component if datas are not fetched

---
+++ src/components/error/Error.jsx
@@ -0,0 +1,13 @@
+
+const Error = (props) => {
+
+  const {dataLife} = props;
+
+  return (
+
+    <div className="error"> Oups! les données demandées ne sont pas disponibles.</div>
+  )
+
+}
+
+export default Error
