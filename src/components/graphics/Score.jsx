41407a18d78c0342e031a9a8178ca2bb6c326ea5
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-26 17:57:30 +0100

    Create Score  component with gestion of not fetching data + use state and effect

--- src/components/graphics/Score.jsx
+++ src/components/graphics/Score.jsx
@@ -1,26 +1,78 @@
+import { useState,useEffect } from 'react';
 import {useOutletContext} from 'react-router-dom'
+
+import Error from '@components/error/Error';
+
+import {PieChart, Pie, Cell,Tooltip, ResponsiveContainer} from 'recharts'
 
 import '@styles/layout/graphics.scss'
 
 const Score = () =>{
 
-const {mockUser}= useOutletContext();
+  const {mockUser}= useOutletContext();
 
-const {todayScore} = mockUser[0];
+  const [dataLife,setDataLife] = useState(false);
+
+  const [dataSets,setDataSets] = useState([]);
+
+
+const finalScore = mockUser[0]?.todayScore || mockUser[0]?.score;
+
+
+useEffect(() => { 
+
+  if (finalScore) {
+
+    setDataLife(dataLife=>!dataLife);
+
+    setDataSets([
+      {
+        "name": "Full",
+        "value": (1 - finalScore) * 100
+      },
+      {
+        "name": "Score",
+        "value": (finalScore) * 100 
+      }
+    ])
+
+  } else {
+
+    console.error('No data available to display');
+
+  }
+
+},[finalScore]);
 
 
   return ( 
   
     <div className="block score">
 
-      <div className="final">
-            {todayScore * 100}%
-      </div>
-  
-    </div>
-  
-)  
+      {finalScore ? (
+        <>
+        <ResponsiveContainer width="100%" height="100%">
+        
+            <PieChart width={900} height={900}>
+              <Pie data={dataSets} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} fill="var(--data-color)"/>
 
+              <Tooltip />
+              
+            </PieChart>
+
+        </ResponsiveContainer>
+
+        <div className="final">
+        {finalScore * 100}%
+        <span className='baseline'>de votre objectif</span>
+        </div> 
+
+        </>) : (<Error dataLife={dataLife} />)
+      }
+
+    </div>
+
+)  
 
 }
 
