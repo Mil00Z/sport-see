ad5b2949e8a71b9001bfae7bd6d5d67d2767c889
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-12-04 12:41:23 +0100

    Daily Component Adaptation of APi / Mock call + useEfect flow

--- src/components/graphics/Daily.jsx
+++ src/components/graphics/Daily.jsx
@@ -1,4 +1,8 @@
-import mockActivity from '@datas/mock/mockUser12Activity.json'
+import {useState,useEffect} from 'react';
+import {useOutletContext} from 'react-router-dom'
+
+import useFetching  from '@root/utils/hooks.jsx'
+import Error from '@components/error/Error';
 
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer} from 'recharts';
 
@@ -6,18 +10,71 @@ import '@styles/layout/graphics.scss'
 
 const Daily = () =>{
 
-  const dataActivity = mockActivity[0];
+  const {mockDatas,userId} = useOutletContext()
 
-  const dataSets = Object.keys(dataActivity.sessions).map(key => dataActivity.sessions[key]);
+  const {dataFetched,isLoaded} = useFetching(`http://localhost:3000/user/${userId}/activity`)
 
-  console.log(dataSets);
+  const [dataSets,setDataSets] = useState([]);
+
 
   let minWeight = Math.min(...dataSets.map((item) => item.kilogram));
 
   let maxKilCal = Math.max(...dataSets.map((item) => item.calories))
 
 
+
+useEffect(() => { 
+
+  if (isLoaded) {
+
+    setDataSets((dataSets) => {
+
+      return dataFetched.data.sessions.map((item) => {
+
+        return {
+          "day": item.day,
+          "calories": item.calories,
+          "kilogram": item.kilogram
+        }
   
+  
+      })
+
+    });
+
+    console.log('data flow : API');
+
+  } else {
+
+    const userLocalData = mockDatas?.USER_ACTIVITY?.find((element) => element.userId === userId)
+
+    if(userLocalData){
+
+      setDataSets((dataSets) => {
+
+        return userLocalData.sessions.map((item) => {
+  
+          return {
+            "day": item.day,
+            "calories": item.calories,
+            "kilogram": item.kilogram
+          }
+    
+        })
+
+      });
+
+      console.log('data flow : Mock');
+
+    } else {
+      console.log('Finally Datas Error')
+    }
+
+  }
+
+},[userId])
+  
+
 const CustomTooltip = ({active,payload}) => {
 
   if (active && payload && payload.length) {
@@ -35,50 +92,45 @@ const CustomTooltip = ({active,payload}) => {
 
 }
 
-const CustomLegend = ({payload}) => {
 
-   console.log(payload);
-
-   return (
-     <ul >
-        {payload.map((item, index) => {
-
-            return (<li key={`${item}-${index}`}>Poids(kg)</li>)
-
-        })}
-     </ul>
-   )
-}
 
   return ( 
   
     <div className="block daily">
 
-      <h2>Activité Quotidienne</h2>
-    
-      <ResponsiveContainer width="100%" height="100%">
-    
-          <BarChart data={dataSets} margin={{ top: 20, right: 20, left:20, bottom:5}} barGap={12} >
 
-            <CartesianGrid strokeDasharray="8" vertical={false} />
+      {dataSets.length !== 0 ? (
+        <>
+         <h2 className='graph-title'>Activité Quotidienne</h2>
 
-            <XAxis dataKey="day" tickLine={false}  />
+         <ResponsiveContainer width="100%" height="100%">
+       
+             <BarChart data={dataSets} margin={{ top: 20, right: 20, left:20, bottom:5}} barGap={12} >
+   
+               <CartesianGrid strokeDasharray="8" vertical={false} />
+   
+               <XAxis dataKey="day" tickLine={false}  />
+   
+               <Bar yAxisId="right" dataKey="kilogram" fill="var(--black-color)" barSize={10} radius={[20,20,0,0]} />
+               <YAxis yAxisId="right" type="number" dataKey="kilogram" orientation="right" domain={[minWeight - 10, 100]} name={"Poids(kg)"} />
+   
+   
+               <Bar yAxisId="left" dataKey="calories" fill="var(--timer-color)" barSize={10} radius={[20,20,0,0]} />
+               <YAxis yAxisId ="left" type="number" dataKey="calories" orientation="left" domain={[0, maxKilCal + 50]} hide={true}  />
+   
+               <Tooltip content={<CustomTooltip />} cursor={{fill: 'rgba( 196 , 196 , 196,.5)'}}/>
+   
+               <Legend align='right' verticalAlign='top' iconType="circle" iconSize={15} height={45} payload={[{value:"Poids(Kg)"},{value:"Calories brulées (Kcal)", color:'var(--timer-color)'}]} />
+               
+             </BarChart>
+   
+         </ResponsiveContainer>
+      </>
 
-            <Bar yAxisId="right" dataKey="kilogram" fill="var(--black-color)" barSize={10} radius={[20,20,0,0]} />
-            <YAxis yAxisId="right" type="number" dataKey="kilogram" orientation="right" domain={[minWeight - 10, 100]} name={"Poids(kg)"} />
+      ):(<Error />)
+    }
 
-
-            <Bar yAxisId="left" dataKey="calories" fill="var(--timer-color)" barSize={10} radius={[20,20,0,0]} />
-            <YAxis yAxisId ="left" type="number" dataKey="calories" orientation="left" domain={[0, maxKilCal + 50]} hide={true}  />
-
-            <Tooltip content={<CustomTooltip />} cursor={{fill: 'rgba( 196 , 196 , 196,.5)'}}/>
-
-            <Legend align='right' verticalAlign='top' iconType="circle" iconSize={15} height={45} payload={[{value:"Poids(Kg)"},{value:"Calories brulées (Kcal)", color:'var(--timer-color)'}]} />
-            
-          </BarChart>
-
-      </ResponsiveContainer>
-     
+   
     </div>
   
 )  
